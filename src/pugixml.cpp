ce0d61c97cf336829fea891541e478003e926184
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2026-03-30 09:36:22 +0200

    [pugi] harden XML helpers

--- src/pugixml.cpp
+++ src/pugixml.cpp
@@ -6,25 +6,37 @@
 #include <memory>
 #include <vector>
 
+// --- Safety Macros ---
+
+#define CHECK_NODE(ptr) \
+    if (ptr == R_NilValue || TYPEOF(ptr) != EXTPTRSXP) \
+        Rf_error("Invalid pugi_node handle: Object is not an external pointer."); \
+    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(ptr); \
+    if (!node) Rf_error("Invalid pugi_node handle: Pointer is NULL.");
+
+#define CHECK_STRING(s, name) \
+    if (!Rf_isString(s) || Rf_length(s) < 1) \
+        Rf_error("Argument '%s' must be a non-empty character string.", name);
+
 extern "C" {
 
   // --- Finalizer ---
-  // This is called by R's Garbage Collector to prevent memory leaks
   void pugi_node_finalizer(SEXP ext_ptr) {
     pugi::xml_node* ptr = (pugi::xml_node*)R_ExternalPtrAddr(ext_ptr);
-    if (ptr) delete ptr;
+    if (ptr) {
+      delete ptr;
+      R_SetExternalPtrAddr(ext_ptr, NULL);
+    }
   }
 
   // --- Internal Helper ---
-  SEXP wrap_node_raw(pugi::xml_node node, bool is_doc = false) {
-    // Allocate handle on heap
-    pugi::xml_node* ptr = new pugi::xml_node(node);
-
-    // Create External Pointer
-    SEXP ext_ptr = PROTECT(R_MakeExternalPtr(ptr, Rf_install("pugi_node"), R_NilValue));
+  SEXP wrap_node_raw(pugi::xml_node node, SEXP prot, bool is_doc = false) {
+    SEXP ext_ptr = PROTECT(R_MakeExternalPtr(NULL, Rf_install("pugi_node"), prot));
     R_RegisterCFinalizerEx(ext_ptr, pugi_node_finalizer, TRUE);
 
-    // Set S3 Classes
+    pugi::xml_node* ptr = new pugi::xml_node(node);
+    R_SetExternalPtrAddr(ext_ptr, ptr);
+
     SEXP cls;
     if (is_doc) {
       cls = PROTECT(Rf_allocVector(STRSXP, 3));
@@ -42,97 +54,33 @@ extern "C" {
     return ext_ptr;
   }
 
-  // --- Exported Functions ---
+  // --- Search & Navigation ---
 
   SEXP pugi_find_first(SEXP node_ptr, SEXP xpath_str) {
-    if (node_ptr == R_NilValue) return R_NilValue;
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    CHECK_NODE(node_ptr)
+    CHECK_STRING(xpath_str, "xpath")
     const char* xpath = CHAR(STRING_ELT(xpath_str, 0));
-
     pugi::xpath_node target = node->select_node(xpath);
-    return wrap_node_raw(target.node());
+    return wrap_node_raw(target.node(), node_ptr);
   }
 
   SEXP pugi_find_all(SEXP node_ptr, SEXP xpath_str) {
-    if (node_ptr == R_NilValue) return Rf_allocVector(VECSXP, 0);
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    CHECK_NODE(node_ptr)
+    CHECK_STRING(xpath_str, "xpath")
     const char* xpath = CHAR(STRING_ELT(xpath_str, 0));
-
     pugi::xpath_node_set nodes = node->select_nodes(xpath);
     int n = nodes.size();
 
     SEXP out = PROTECT(Rf_allocVector(VECSXP, n));
     for (int i = 0; i < n; i++) {
-      SET_VECTOR_ELT(out, i, wrap_node_raw(nodes[i].node()));
+      SET_VECTOR_ELT(out, i, wrap_node_raw(nodes[i].node(), node_ptr));
     }
-
     UNPROTECT(1);
     return out;
   }
 
-  SEXP pugi_add_child(SEXP node_ptr, SEXP input, SEXP where_int) {
-    if (node_ptr == R_NilValue || TYPEOF(node_ptr) != EXTPTRSXP) {
-      return R_NilValue;
-    }
-
-    pugi::xml_node* parent_node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
-    if (!parent_node) return R_NilValue;
-
-    int where = Rf_asInteger(where_int);
-    pugi::xml_node last_added;
-
-    // Input is a List (Result of xml_find_all)
-    if (TYPEOF(input) == VECSXP) {
-      int n = Rf_length(input);
-
-      for (int i = 0; i < n; i++) {
-        SEXP item = VECTOR_ELT(input, i);
-
-        if (TYPEOF(item) == EXTPTRSXP) {
-          pugi::xml_node* input_node = (pugi::xml_node*)R_ExternalPtrAddr(item);
-          if (input_node) {
-            if (where == 0) last_added = parent_node->prepend_copy(*input_node);
-            else last_added = parent_node->append_copy(*input_node);
-          }
-        }
-      }
-      return last_added ? wrap_node_raw(last_added) : node_ptr;
-    }
-
-    // Input is a Single External Pointer (Result of xml_find_first)
-    if (TYPEOF(input) == EXTPTRSXP) {
-      pugi::xml_node* input_node = (pugi::xml_node*)R_ExternalPtrAddr(input);
-      if (!input_node) return R_NilValue;
-
-      if (where == 0) last_added = parent_node->prepend_copy(*input_node);
-      else last_added = parent_node->append_copy(*input_node);
-
-      return wrap_node_raw(last_added);
-    }
-
-    // Input is a String (Create a new empty tag by name)
-    if (Rf_isString(input) && Rf_length(input) > 0) {
-      const char* name = CHAR(STRING_ELT(input, 0));
-
-      if (where == 0) last_added = parent_node->prepend_child(name);
-      else last_added = parent_node->append_child(name);
-
-      return wrap_node_raw(last_added);
-    }
-
-    return R_NilValue;
-  }
-
-  SEXP pugi_remove(SEXP node_ptr) {
-    if (node_ptr == R_NilValue) return R_NilValue;
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
-    if (node->parent()) node->parent().remove_child(*node);
-    return R_NilValue;
-  }
-
   SEXP pugi_children(SEXP node_ptr) {
-    if (node_ptr == R_NilValue) return Rf_allocVector(VECSXP, 0);
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    CHECK_NODE(node_ptr)
     pugi::xml_node target = *node;
 
     if (target.type() == pugi::node_document) {
@@ -151,7 +99,7 @@ extern "C" {
 
     SEXP out = PROTECT(Rf_allocVector(VECSXP, children.size()));
     for (size_t i = 0; i < children.size(); i++) {
-      SET_VECTOR_ELT(out, i, wrap_node_raw(children[i]));
+      SET_VECTOR_ELT(out, i, wrap_node_raw(children[i], node_ptr));
     }
 
     SEXP cls = PROTECT(Rf_allocVector(STRSXP, 2));
@@ -164,8 +112,7 @@ extern "C" {
   }
 
   SEXP pugi_node_length(SEXP node_ptr) {
-    if (node_ptr == R_NilValue) return Rf_ScalarInteger(0);
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    CHECK_NODE(node_ptr)
     pugi::xml_node target = *node;
 
     if (target.type() == pugi::node_document) {
@@ -184,58 +131,110 @@ extern "C" {
     return Rf_ScalarInteger(count);
   }
 
+  // --- Modification ---
+
+  SEXP pugi_add_child(SEXP node_ptr, SEXP input, SEXP where_int) {
+    CHECK_NODE(node_ptr)
+    int where = Rf_asInteger(where_int);
+    pugi::xml_node last_added;
+
+    if (TYPEOF(input) == VECSXP) {
+      int n = Rf_length(input);
+      for (int i = 0; i < n; i++) {
+        SEXP item = VECTOR_ELT(input, i);
+        if (TYPEOF(item) == EXTPTRSXP) {
+          pugi::xml_node* input_node = (pugi::xml_node*)R_ExternalPtrAddr(item);
+          if (input_node && *input_node) {
+            last_added = (where == 0) ? node->prepend_copy(*input_node) : node->append_copy(*input_node);
+          }
+        }
+      }
+      return last_added ? wrap_node_raw(last_added, node_ptr) : node_ptr;
+    }
+
+    if (TYPEOF(input) == EXTPTRSXP) {
+      pugi::xml_node* input_node = (pugi::xml_node*)R_ExternalPtrAddr(input);
+      if (!input_node || !(*input_node)) return R_NilValue;
+      last_added = (where == 0) ? node->prepend_copy(*input_node) : node->append_copy(*input_node);
+      return wrap_node_raw(last_added, node_ptr);
+    }
+
+    if (Rf_isString(input) && Rf_length(input) > 0) {
+      const char* name = CHAR(STRING_ELT(input, 0));
+      last_added = (where == 0) ? node->prepend_child(name) : node->append_child(name);
+      return wrap_node_raw(last_added, node_ptr);
+    }
+
+    return R_NilValue;
+  }
+
+  SEXP pugi_remove(SEXP node_ptr) {
+    CHECK_NODE(node_ptr)
+    if (node->parent()) node->parent().remove_child(*node);
+    return R_NilValue;
+  }
+
+  // --- Attributes & Text ---
+
   SEXP pugi_set_attr(SEXP node_ptr, SEXP name_str, SEXP val_str) {
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    CHECK_NODE(node_ptr)
+    CHECK_STRING(name_str, "name")
+    CHECK_STRING(val_str, "value")
     const char* name = CHAR(STRING_ELT(name_str, 0));
     const char* val = CHAR(STRING_ELT(val_str, 0));
 
     pugi::xml_attribute attr = node->attribute(name);
     if (attr) attr.set_value(val);
     else node->append_attribute(name).set_value(val);
-
     return R_NilValue;
   }
 
   SEXP pugi_get_attr(SEXP node_ptr, SEXP name_str) {
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    CHECK_NODE(node_ptr)
+    CHECK_STRING(name_str, "name")
     const char* name = CHAR(STRING_ELT(name_str, 0));
     return Rf_mkString(node->attribute(name).value());
   }
 
+  SEXP pugi_has_attr(SEXP node_ptr, SEXP name_str) {
+    CHECK_NODE(node_ptr)
+    CHECK_STRING(name_str, "name")
+    const char* name = CHAR(STRING_ELT(name_str, 0));
+    return Rf_ScalarLogical(!node->attribute(name).empty());
+  }
+
   SEXP pugi_set_text(SEXP node_ptr, SEXP text_str) {
-    if (Rf_isNull(node_ptr)) return R_NilValue;
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    CHECK_NODE(node_ptr)
+    CHECK_STRING(text_str, "text")
     const char* text = CHAR(STRING_ELT(text_str, 0));
     node->text().set(text);
     return R_NilValue;
   }
 
+  // --- Metadata & Serialization ---
+
   SEXP pugi_node_name(SEXP node_ptr) {
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    CHECK_NODE(node_ptr)
     return Rf_mkString(node->name());
   }
 
   SEXP pugi_serialize_node(SEXP node_ptr) {
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
-    if (!node || !(*node)) return Rf_mkString("");
+    pugi::xml_node* ptr = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    if (!ptr || !(*ptr)) return Rf_mkString("");
 
     std::stringstream ss;
-    node->print(ss, "", pugi::format_raw | pugi::format_no_declaration);
+    ptr->print(ss, "", pugi::format_raw | pugi::format_no_declaration);
     return Rf_mkString(ss.str().c_str());
   }
 
   SEXP pugi_node_type(SEXP node_ptr) {
-    if (node_ptr == R_NilValue) return Rf_mkString("missing");
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
-    if (node->type() == pugi::node_document) return Rf_mkString("document");
-    if (node->type() == pugi::node_element) return Rf_mkString("element");
-    return Rf_mkString("other");
-  }
+    if (node_ptr == R_NilValue || TYPEOF(node_ptr) != EXTPTRSXP) return Rf_mkString("missing");
+    pugi::xml_node* ptr = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
+    if (!ptr || !(*ptr)) return Rf_mkString("missing");
 
-  SEXP pugi_has_attr(SEXP node_ptr, SEXP name_str) {
-    pugi::xml_node* node = (pugi::xml_node*)R_ExternalPtrAddr(node_ptr);
-    const char* name = CHAR(STRING_ELT(name_str, 0));
-    return Rf_ScalarLogical(!node->attribute(name).empty());
+    if (ptr->type() == pugi::node_document) return Rf_mkString("document");
+    if (ptr->type() == pugi::node_element) return Rf_mkString("element");
+    return Rf_mkString("other");
   }
 
 } // extern "C"
