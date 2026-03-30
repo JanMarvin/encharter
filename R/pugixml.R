f23e9684fab8b9aca21b79715f63341822f3af9f
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2026-03-30 09:51:20 +0200

    [pugi] document internal helper functions

--- R/pugixml.R
+++ R/pugixml.R
@@ -1,3 +1,15 @@
+#' Add a child node to an XML target
+#'
+#' @param .x A pugi_node, pugi_xml, or a list containing one.
+#' @param .name The name of the new tag to create.
+#' @param ... Named arguments for attributes, unnamed for text content.
+#' @param .where Integer; 0 to prepend, -1 to append.
+#' @param .value Optional character string to set as text content.
+#' @return The newly created pugi_node.
+#' @keywords internal
+#' @examples
+#' # doc <- read_xml("<root><child/></root>")
+#' # xml_add_child(doc, "new_node", id = "1", "Some text")
 xml_add_child <- function(.x, .name, ..., .where = -1, .value = NULL) {
   target <- if (is.list(.x)) .x[[1]] else .x
 
@@ -27,12 +39,28 @@ xml_add_child <- function(.x, .name, ..., .where = -1, .value = NULL) {
   new_node
 }
 
+#' Find first match via XPath
+#'
+#' @param x A pugi_node or list of nodes.
+#' @param xpath Character string containing XPath expression.
+#' @return A pugi_node or list of nodes.
+#' @keywords internal
+#' @examples
+#' # xml_find_first(doc, ".//sheetData")
 xml_find_first <- function(x, xpath) {
   if (is.list(x)) return(lapply(x, xml_find_first, xpath = xpath))
   if (!grepl("^\\.|^/", xpath)) xpath <- paste0(".//", xpath)
   .Call("pugi_find_first", x, xpath)
 }
 
+#' Find all matches via XPath
+#'
+#' @param x A pugi_node or list of nodes.
+#' @param xpath Character string containing XPath expression.
+#' @return A pugi_nodeset (list of pugi_nodes).
+#' @keywords internal
+#' @examples
+#' # xml_find_all(doc, ".//row")
 xml_find_all <- function(x, xpath) {
   if (is.list(x)) {
     res <- unlist(lapply(x, xml_find_all, xpath = xpath), recursive = FALSE)
@@ -43,6 +71,13 @@ xml_find_all <- function(x, xpath) {
   .Call("pugi_find_all", x, xpath)
 }
 
+#' Get element children
+#'
+#' @param x A pugi_node or list of nodes.
+#' @return A pugi_nodeset of child elements.
+#' @keywords internal
+#' @examples
+#' # xml_children(xml_find_first(doc, ".//row"))
 xml_children <- function(x) {
   if (is.list(x)) {
     res <- unlist(lapply(x, function(node) .Call("pugi_children", node)), recursive = FALSE)
@@ -52,21 +87,49 @@ xml_children <- function(x) {
   .Call("pugi_children", x)
 }
 
+#' Get node names
+#'
+#' @param x A pugi_node or list of nodes.
+#' @return A character vector of tag names.
+#' @keywords internal
+#' @examples
+#' # xml_name(xml_find_all(doc, "/*/*"))
 xml_name <- function(x) {
   if (is.list(x)) return(unname(sapply(x, function(node) .Call("pugi_node_name", node))))
   .Call("pugi_node_name", x)
 }
 
+#' Get node types
+#'
+#' @param x A pugi_node or list of nodes.
+#' @return A character vector (e.g., "element", "document").
+#' @keywords internal
 xml_type <- function(x) {
   if (is.list(x)) return(unname(sapply(x, function(node) .Call("pugi_node_type", node))))
   .Call("pugi_node_type", x)
 }
 
+#' Get attribute value
+#'
+#' @param x A pugi_node or list of nodes.
+#' @param attr Character string of the attribute name.
+#' @return A character vector of attribute values.
+#' @keywords internal
+#' @examples
+#' # xml_attr(rows, "r")
 xml_attr <- function(x, attr) {
   if (is.list(x)) return(unname(sapply(x, function(node) .Call("pugi_get_attr", node, attr))))
   .Call("pugi_get_attr", x, attr)
 }
 
+#' Set attribute value
+#'
+#' @param x A pugi_node or list of nodes.
+#' @param attr Character string of the attribute name.
+#' @param value The value to set (coerced to character).
+#' @keywords internal
+#' @examples
+#' # xml_set_attr(rows, "customHeight", "1")
 xml_set_attr <- function(x, attr, value) {
   if (is.list(x)) {
     invisible(lapply(x, function(node) .Call("pugi_set_attr", node, attr, as.character(value))))
@@ -75,16 +138,37 @@ xml_set_attr <- function(x, attr, value) {
   }
 }
 
+#' Check for attribute existence
+#'
+#' @param x A pugi_node or list of nodes.
+#' @param attr Character string of the attribute name.
+#' @return A logical vector.
+#' @keywords internal
+#' @examples
+#' # has_r <- xml_has_attr(cells, "r")
 xml_has_attr <- function(x, attr) {
   if (is.list(x)) return(unname(sapply(x, function(node) .Call("pugi_has_attr", node, attr))))
   .Call("pugi_has_attr", x, attr)
 }
 
+#' Get count of child elements
+#'
+#' @param x A pugi_node or list of nodes.
+#' @return An integer vector of child counts.
+#' @keywords internal
+#' @examples
+#' # xml_length(doc)
 xml_length <- function(x) {
   if (is.list(x)) return(unname(sapply(x, function(node) .Call("pugi_node_length", node))))
   .Call("pugi_node_length", x)
 }
 
+#' Remove nodes from the tree
+#'
+#' @param x A pugi_node or list of nodes.
+#' @keywords internal
+#' @examples
+#' # xml_remove(xml_find_all(doc, ".//legacyDrawing"))
 xml_remove <- function(x) {
   if (is.list(x)) invisible(lapply(x, function(node) .Call("pugi_remove", node)))
   else if (!is.null(x)) .Call("pugi_remove", x)
